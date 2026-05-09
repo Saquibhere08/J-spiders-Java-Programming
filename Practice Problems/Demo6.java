0f73771cabf6c4855035889b0b63b8a9fbad0c13
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-05-10 00:35:41 +0530

    Add files via upload

---
+++ Practice Problems/Demo6.java
@@ -0,0 +1,11 @@
+package PracticeQuestions;
+
+public class Demo6 {
+    public static void main(String[] args) {
+        //WAJP to print an Infinite loop using Do-While Loop.
+        int i=1;
+        do{
+            System.out.println("This is An  Infinite Loop.");
+        }while(i >0);
+    }
+}
